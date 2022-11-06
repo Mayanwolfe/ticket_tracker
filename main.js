b0a0324e8d73c18bfd29b29753c8c7f0577bc390
Claire Bourdon <cebourdon625@gmail.com>
2022-11-06 16:50:34 -0500

    building functions

---
+++ main.js
@@ -0,0 +1,40 @@
+document.getElementById('issueInputForm').addEventListener('submit',saveIssue)
+
+function fetchIssues () {
+    let issues = JSON.parse(localStorage.getItem('issues'))
+    let issuesList = document.getElementById('issuesList')
+
+    issuesList.innerHTML = '';
+
+    for (let i = 0; i < issues.length; i++) {
+        let id = issues[i].id
+        let subject = issues[i].subject
+        let description = issues[i].description
+        let severity = issues[i].severity
+        let assignedTo = issues[i].assignedTo
+        let status = issues[i].status
+        let statusColor = status == "Closed" ? 'label-success' : 'label-info'
+
+        //issuesList.innerHTML +=
+    }
+}
+
+function saveIssue(e) {
+    let issueId = chance.guid()
+    let issueSubject = document.getElementById('issueSubjInput').value
+    let issueDesc = document.getElementById('issueDescInput').value
+    let issueSeverity = document.getElementById('issueSeverityInput').value
+    let issueAssignedTo = document.getElementById('issueAssignedToInput').value
+    let issueStatus = 'Open'
+
+    let issue = {
+        id: issueId,
+        subject: issueSubject,
+        description: issueDesc,
+        severity: issueSeverity,
+        assignedTo: issueAssignedTo,
+        status: issueStatus
+    }
+
+    
+}
